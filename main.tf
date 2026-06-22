5f0b56747df8bc0a5cdd81816c4e3719e9cc40b2
Binaydevops <binaydevops@gmail.com>
2026-06-22 23:19:52 +0530

    added storage account configuration

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
 resource "azurerm_resource_group" "this" {
-  name      = "rg-binay"
+  name     = "rg-binay"
   location = "centralindia"
 }
 
+resource "azurerm_storage_account" "this" {
+  name                     = "storageabinay"
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
